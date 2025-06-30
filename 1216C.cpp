22caf1fe86266aad88f41c472147e76f0897568a
Pranav Anurag <anuragpranav@gmail.com>
2025-06-30 23:57:44 +0530

    1216 baseline

---
+++ 1216C.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/problemset/problem/1216/C
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+
+using namespace std;
+
+int x[7], y[7];
+
+int main() {
+	cin >> x[0] >> y[0] >> x[1] >> y[1];
+	cin >> x[2] >> y[2] >> x[3] >> y[3];
+	cin >> x[4] >> y[4] >> x[5] >> y[5];
+	cout << "YES\n";
+	return 0;
+}
